daf24ed0649651c711a9cd2ff51b6e07ed27970f
sparkler001 <40640865+sparkler001@users.noreply.github.com>
2019-04-26 12:58:06 -0700

    Add files via upload

---
+++ Buildings.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset version="1.2" tiledversion="1.2.3" name="Buildings" tilewidth="484" tileheight="484" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="376" height="484"/>
+ <tile id="0">
+  <image width="336" height="354" source="Buildings/house1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="302" height="348" source="Buildings/house2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="352" height="395" source="Buildings/house3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="376" height="484" source="Buildings/house-mill.png"/>
+ </tile>
+ <tile id="4">
+  <image width="253" height="234" source="Buildings/stall.png"/>
+ </tile>
+ <tile id="5">
+  <image width="484" height="377" source="Buildings/house4.png"/>
+ </tile>
+</tileset>
