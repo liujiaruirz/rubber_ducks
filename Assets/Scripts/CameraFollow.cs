6997ca6b11f509a38cce97a751788b247cd7f7d6
Jerry Liu <jiaruirz@gmail.com>
2019-05-05 15:08:54 -0700

    With interior

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Camera mycam;
+    public float m_speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mycam = GetComponent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mycam.orthographicSize = (Screen.height / 25f) / 2f;
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, m_speed) + new Vector3(0,0,-10);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    Rigidbody2D rbody;
+    Animator anim;
+    public float Move_Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (movement_vector != Vector2.zero)
+        {
+            anim.SetBool("is_walking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+        }
+        else
+        {
+            anim.SetBool("is_walking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * Move_Speed* Time.deltaTime);
+
+    }
+}
